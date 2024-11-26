202a24a1f753ec80e86b2aac4f50efb5decad7d4
Harm Tilburgs <htilburgs@live.com>
2024-11-26 21:27:29 +0100

    Create node_helper.js

---
+++ node_helper.js
@@ -0,0 +1,47 @@
+/*
+//-------------------------------------------
+MMM-MyHomeWizard
+Copyright (C) 2024 - H. Tilburgs
+MIT License
+
+v1.0.0 : Initial version
+
+//-------------------------------------------
+*/
+
+const NodeHelper = require('node_helper');
+
+module.exports = NodeHelper.create({
+
+  start: function() {
+          console.log("Starting node_helper for: " + this.name);
+  },
+
+getMHW: function(url) {
+        // Make a GET request using the Fetch API
+        fetch(url)
+          .then(response => {
+            if (!response.ok) {
+              console.error('MMM-MyHomeWizard: Network response was not ok');
+            }
+            return response.json();
+          })
+
+          .then(result => {
+            // Process the retrieved user data
+            // console.log(result.liveweer['0']); // Remove trailing slashes to display data in Console for testing
+            this.sendSocketNotification('MHW_RESULT', result);
+          })
+
+          .catch(error => {
+            console.error('Error:', error);
+          });
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+            if (notification === 'GET_MHW') {
+            this.getMHW(payload);
+            }
+  },
+
+});
