890434cd06b29f8513737845a3ae01660a2cede3
Harm Tilburgs <htilburgs@live.com>
2026-01-15 12:21:00 +0100

    Update MMM-MyHomeWizard.js

--- MMM-MyHomeWizard.js
+++ MMM-MyHomeWizard.js
@@ -1,17 +1,14 @@
 Module.register('MMM-MyHomeWizard', {
 
     defaults: {
-        P1_IP: null,
-        WM_IP: null,
         maxWidth: "500px",
         extraInfo: false,
-        showFooter: false,
+        showFooter: true,
         showGas: true,
         showFeedback: true,
-        currentPower: false,
-        currentWater: false,
-        initialLoadDelay: 1000,
-        updateInterval: 10000
+        currentPower: true,
+        currentWater: true,
+        initialLoadDelay: 1000
     },
 
     getStyles: function() {
@@ -19,44 +16,17 @@ Module.register('MMM-MyHomeWizard', {
     },
 
     getTranslations: function() {
-        return {
-            nl: "translations/nl.json",
-            en: "translations/en.json"
-        };
+        return { nl: "translations/nl.json", en: "translations/en.json" };
     },
 
     start: function() {
         Log.info("Starting module: " + this.name);
-        this.requiresVersion = "2.9.0";
 
-        this.urlP1 = this.config.P1_IP ? "http://" + this.config.P1_IP + "/api/v1/data/" : "https://dummyjson.com/c/f8b2-91c3-400b-8709";
-        this.urlWM = this.config.WM_IP ? "http://" + this.config.WM_IP + "/api/v1/data/" : "https://dummyjson.com/c/704a-9a96-4845-bc72";
-
-        this.MHW_P1 = [];
-        this.MHW_WM = [];
-        this.dailyUsage = null;
+        this.MHW_P1 = {};
+        this.MHW_WM = {};
+        this.dailyUsage = {};
         this.loadedP1 = false;
         this.loadedWM = false;
-
-        this.scheduleUpdate();
-    },
-
-    scheduleUpdate: function() {
-        setInterval(() => {
-            this.getMHW_P1();
-            this.getMHW_WM();
-        }, this.config.updateInterval);
-
-        this.getMHW_P1();
-        this.getMHW_WM();
-    },
-
-    getMHW_P1: function() {
-        this.sendSocketNotification('GET_MHWP1', this.urlP1);
-    },
-
-    getMHW_WM: function() {
-        this.sendSocketNotification('GET_MHWWM', this.urlWM);
     },
 
     socketNotificationReceived: function(notification, payload) {
@@ -64,11 +34,13 @@ Module.register('MMM-MyHomeWizard', {
             this.MHW_P1 = payload;
             this.loadedP1 = true;
             this.updateDom(this.config.initialLoadDelay);
-        } else if (notification === "MHWWM_RESULT") {
+        }
+        else if (notification === "MHWWM_RESULT") {
             this.MHW_WM = payload;
             this.loadedWM = true;
             this.updateDom(this.config.initialLoadDelay);
-        } else if (notification === "DAILY_USAGE") {
+        }
+        else if (notification === "DAILY_USAGE") {
             this.dailyUsage = payload;
             this.updateDom(this.config.initialLoadDelay);
         }
@@ -86,7 +58,7 @@ Module.register('MMM-MyHomeWizard', {
         wrapper.className = "wrapper";
         wrapper.style.maxWidth = this.config.maxWidth;
 
-        if ((!this.loadedP1 && this.config.P1_IP) || (!this.loadedWM && this.config.WM_IP)) {
+        if (!this.loadedP1 || !this.loadedWM) {
             wrapper.innerHTML = "Loading....";
             wrapper.classList.add("bright", "light", "small");
             return wrapper;
@@ -95,9 +67,19 @@ Module.register('MMM-MyHomeWizard', {
         const table = document.createElement("table");
         table.className = "small";
 
-        // --- Actuele data rijen toevoegen hier (zoals eerder) ---
+        // --- Actuele P1 data ---
+        const rowP1 = document.createElement("tr");
+        rowP1.appendChild(this.createCell("Total Power", "textcell"));
+        rowP1.appendChild(this.createCell(this.MHW_P1.total_power_import_kwh + " kWh", "datacell"));
+        table.appendChild(rowP1);
 
-        // --- DAGELIJKS VERBRUIK ---
+        // --- Actuele Water data ---
+        const rowWM = document.createElement("tr");
+        rowWM.appendChild(this.createCell("Total Water", "textcell"));
+        rowWM.appendChild(this.createCell(this.MHW_WM.total_liter_m3 + " m³", "datacell"));
+        table.appendChild(rowWM);
+
+        // --- Dagelijks verbruik ---
         if (this.dailyUsage) {
             const items = [
                 {label: "Daily_Electricity", value: this.dailyUsage.electricity_kwh, unit: "kWh"},
@@ -109,8 +91,8 @@ Module.register('MMM-MyHomeWizard', {
             items.forEach(item => {
                 if (item.value !== undefined) {
                     const row = document.createElement("tr");
-                    row.appendChild(this.createCell('<i class="fa-solid fa-clock"></i>&nbsp;' + this.translate(item.label), "dailytextcell"));
-                    row.appendChild(this.createCell(Math.round(item.value * 100) / 100 + " " + item.unit, "dailydatacell"));
+                    row.appendChild(this.createCell(item.label, "dailytextcell"));
+                    row.appendChild(this.createCell(Math.round(item.value*100)/100 + " " + item.unit, "dailydatacell"));
                     table.appendChild(row);
                 }
             });
