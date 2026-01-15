c2e8329086d6e9a450843d4f2e6db3fbdb54ce04
Harm Tilburgs <htilburgs@live.com>
2026-01-15 12:37:49 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,15 +1,12 @@
 const NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
-
     start: function() {
         console.log("NodeHelper started for MMM-MyHomeWizard");
 
-        // Verstuur test data na 1 seconde
         setTimeout(() => {
             console.log("NodeHelper sending test data");
             this.sendSocketNotification("TEST_NOTIFICATION", {foo: "bar"});
         }, 1000);
     }
-
 });
