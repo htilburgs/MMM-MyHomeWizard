a893e8b50d95c1948dbd14546cfa4419f98b4223
Harm Tilburgs <htilburgs@live.com>
2026-01-15 12:29:14 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,23 +1,25 @@
-const NodeHelper = require('node_helper');
-const fs = require('fs');
-const path = require('path');
+const NodeHelper = require("node_helper");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = NodeHelper.create({
 
-    start: function () {
-        console.log("Starting node_helper for: " + this.name);
+    start: function() {
+        console.log("NodeHelper started for MMM-MyHomeWizard");
 
         this.lastP1 = {};
         this.lastWM = {};
 
-        // Verzenden van mock data met voldoende vertraging
-        this.sendMockDataWithDelay();
+        // Verstuur mock data naar frontend na 2 seconden
+        setTimeout(() => {
+            this.sendMockData();
+        }, 2000);
 
-        // Dagelijkse opslag om 12:05
+        // Plan dagelijkse opslag om 12:05
         this.scheduleDailySaveAtNoon();
     },
 
-    sendMockDataWithDelay: function() {
+    sendMockData: function() {
         const mockP1 = {
             total_power_import_kwh: 1234.56,
             total_power_export_kwh: 12.34,
@@ -34,17 +36,16 @@ module.exports = NodeHelper.create({
             wifi_strength: 85
         };
 
-        // Gebruik arrow function zodat 'this' correct blijft
-        setTimeout(() => {
-            console.log("Sending mock data to frontend");
-            this.lastP1 = mockP1;
-            this.lastWM = mockWM;
+        console.log("NodeHelper sending mock P1 & WM data");
 
-            this.sendSocketNotification('MHWP1_RESULT', mockP1);
-            this.sendSocketNotification('MHWWM_RESULT', mockWM);
+        this.lastP1 = mockP1;
+        this.lastWM = mockWM;
 
-            this.sendSocketNotification('DAILY_USAGE', this.calculateDailyConsumptionMock());
-        }, 2000); // 2 seconden vertraging zodat frontend klaar is
+        this.sendSocketNotification("MHWP1_RESULT", mockP1);
+        this.sendSocketNotification("MHWWM_RESULT", mockWM);
+
+        // Stuur ook dagelijks verbruik mock
+        this.sendSocketNotification("DAILY_USAGE", this.calculateDailyConsumptionMock());
     },
 
     calculateDailyConsumptionMock: function() {
@@ -60,7 +61,7 @@ module.exports = NodeHelper.create({
         const now = new Date();
         const next = new Date();
         next.setHours(12, 5, 0, 0);
-        if (now > next) next.setDate(next.getDate() + 1);
+        if(now > next) next.setDate(next.getDate() + 1);
 
         const msUntilNext = next - now;
 
@@ -68,24 +69,24 @@ module.exports = NodeHelper.create({
             this.saveDataAndCalculate();
             setInterval(() => {
                 this.saveDataAndCalculate();
-            }, 24 * 60 * 60 * 1000);
+            }, 24*60*60*1000);
         }, msUntilNext);
     },
 
     saveDataAndCalculate: function() {
         this.saveDataToFile({ MHW_P1: this.lastP1, MHW_WM: this.lastWM });
-        this.sendSocketNotification('DAILY_USAGE', this.calculateDailyConsumptionMock());
+        this.sendSocketNotification("DAILY_USAGE", this.calculateDailyConsumptionMock());
     },
 
     saveDataToFile: function(data) {
         try {
-            const folderPath = path.resolve(__dirname, 'data');
-            if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath);
+            const folderPath = path.resolve(__dirname, "data");
+            if(!fs.existsSync(folderPath)) fs.mkdirSync(folderPath);
 
-            const filePath = path.join(folderPath, 'meter_history.json');
+            const filePath = path.join(folderPath, "meter_history.json");
 
             let history = [];
-            if (fs.existsSync(filePath)) {
+            if(fs.existsSync(filePath)) {
                 const fileContent = fs.readFileSync(filePath);
                 history = JSON.parse(fileContent);
             }
@@ -99,7 +100,7 @@ module.exports = NodeHelper.create({
             history.push(entry);
             fs.writeFileSync(filePath, JSON.stringify(history, null, 2));
             console.log("MMM-MyHomeWizard: Data saved to meter_history.json");
-        } catch (err) {
+        } catch(err) {
             console.error("MMM-MyHomeWizard: Error saving data", err);
         }
     }
