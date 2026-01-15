099fb833ca3dd8faa9bd34961dd9e507d632d8c4
Harm Tilburgs <htilburgs@live.com>
2026-01-15 12:38:22 +0100

    Update MMM-MyHomeWizard.js

--- MMM-MyHomeWizard.js
+++ MMM-MyHomeWizard.js
@@ -1,5 +1,4 @@
 Module.register("MMM-MyHomeWizard", {
-
     start: function() {
         console.log("Module started: MMM-MyHomeWizard");
         this.loaded = false;
@@ -7,8 +6,7 @@ Module.register("MMM-MyHomeWizard", {
     },
 
     socketNotificationReceived: function(notification, payload) {
-        console.log("Frontend received notification:", notification, payload);
-
+        console.log("Frontend received:", notification, payload);
         if(notification === "TEST_NOTIFICATION") {
             this.testData = payload;
             this.loaded = true;
@@ -18,15 +16,12 @@ Module.register("MMM-MyHomeWizard", {
 
     getDom: function() {
         const wrapper = document.createElement("div");
-
         if(!this.loaded) {
             wrapper.innerHTML = "Loading...";
             wrapper.classList.add("bright", "light", "small");
             return wrapper;
         }
-
         wrapper.innerHTML = `Test data received: foo = ${this.testData.foo}`;
         return wrapper;
     }
-
 });
